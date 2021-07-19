9de3597712a04e21b91c57b32031b47a5442cb28
rdhariwa <rdhariwa@sfu.ca>
2021-07-19 13:19:06 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import sys
+import settings
+
+if __name__ == "__main__":
+    if sys.argv[1] == "youtube":
+        from ytmusicapi import YTMusic
+        api = YTMusic()
+        settings['youtube']['headers'] = api.setup()
+    settings.save()
+    
